253eeb67b9ae269d8977cab7823525d412871801
a35219410-gif <a35219410@gmail.com>
2026-05-27 21:25:01 +0300

    upload helpers/get_create.py

---
+++ helpers/get_create.py
@@ -0,0 +1,67 @@
+
+'''
+
+from getids import get_date_as_string
+
+
+
+def get_creation_date(id: int) -> str:
+	if str(id)[0] == '5' and str(id)[1] == '9':
+		return '01/2023'
+	if len(str(id)) == 10:
+		if str(id)[0] == '5':
+			if not str(id)[1] == '0':
+				return '0{}/2022'.format(str(id).replace("0","")[2])
+			else:
+				return '0{}/2022'.format(str(id)[2])
+		elif str(id)[0] == '1' and str(id)[1] == '0':
+			if '11' in str(id):
+				return '11/2020'
+			if '12' in str(id):
+				return '12/2020'
+			else:
+				return '0{}/2020'.format(str(id).replace("0","")[2])
+		else:
+			if '11' in str(id):
+				return '11/2021'
+			if '12' in str(id):
+				return '12/2021'
+			if '10' in str(id):
+				return '10/2021'
+			else:
+				return '0{}/2021'.format(str(id).replace("0","")[1])
+	if len(str(id)) == 9:
+		if str(id)[0] == '9':
+			return '0{}/2020'.format(str(id).replace("0","")[0])
+		else:
+			return get_date_as_string(id)[1]
+	else:
+		return get_date_as_string(id)[1]
+'''
+
+import aiohttp
+from config import r
+
+async def get_creation_date(id: int) -> str:
+    cached = r.get(f'{id}:CreateDate')
+    if cached:
+        return cached
+    url = "https://restore-access.indream.app/regdate"
+    headers = {
+        "accept": "*/*",
+        "content-type": "application/x-www-form-urlencoded",
+        "user-agent": "Nicegram/92 CFNetwork/1390 Darwin/22.0.0",
+        "x-api-key": "e758fb28-79be-4d1c-af6b-066633ded128",
+        "accept-language": "en-US,en;q=0.9"
+    }
+    data = {"telegramId": id}
+    timeout = aiohttp.ClientTimeout(total=15)
+    try:
+        async with aiohttp.ClientSession(timeout=timeout) as session:
+            async with session.post(url, headers=headers, json=data) as resp:
+                result = await resp.json()
+        date = result['data']['date'].replace('-', '/')
+        r.set(f'{id}:CreateDate', date)
+        return date
+    except (KeyError, TypeError, Exception):
+        return 'غير معروف'
